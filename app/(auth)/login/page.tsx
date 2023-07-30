21409138587456f6090c59546fc73b6704af6e0b
Nicolas <nickogarces@gmail.com>
2023-07-30 14:01:06 -0400

    input component + changes to login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,3 +1,10 @@
 export default function LoginPage() {
-    return <div>hola</div>
+    return <section className="grid lg:grid-cols-2 h-screen">
+        <div className="h-screen bg-purple-primary"></div>
+        <div className="flex flex-col justify-center items-center">
+            <div>
+                <h3 className="text-2xl">Hola</h3>
+            </div>
+        </div>
+    </section>
 }
